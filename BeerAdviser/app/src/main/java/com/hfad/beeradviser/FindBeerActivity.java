26aaaafee9b236b9e1fe12b432fc05224bc7b3bb
Emanuele <epatata@hotmail.com>
2017-04-19 23:13:50 +0200

    varie

--- BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +21,12 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+        ImageView photo =  (ImageView) findViewById(R.id.imageView);
+        String description ="this is the logo";
+        int image = R.drawable.apple_logo_318_40184;
+        photo.setImageResource(image);
+        photo.setContentDescription(description);
+
     }
 
     public void onClickFindBeer(View view)
@@ -37,5 +44,18 @@ public class FindBeerActivity extends AppCompatActivity {
         }
 
         brands.setText(brandsFormatted);
+    }
+
+    public void onClickShowLinearLayout(View view)
+    {
+        onClickSwitch(view);
+    }
+
+    public void onClickSwitch(View view)
+    {
+        Intent intent = new Intent(this,LinearLayoutActivity.class);
+        startActivity(intent);
+        //Toast.makeText(this,"ciao",Toast.LENGTH_LONG).show();
+
     }
 }
